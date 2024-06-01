4304c1077bfca5ae3f9880ce3fb0b91b0178caa4
javier <148455736+fjzamora93@users.noreply.github.com>
2024-06-01 20:58:29 +0200

    update

--- tutorial-angular/src/app/user/user.component.ts
+++ tutorial-angular/src/app/user/user.component.ts
@@ -1,10 +1,8 @@
 
-//Signals es una forma evolucionada de detectar eventos, frente a la clásica forma de hacerlo con métodos.
-import { Component, signal, computed } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
 
 @Component({
   selector: 'app-user',
@@ -14,22 +12,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  
-  /*MÉTODO CLÁSICO (hace verificaciones manuales):
-   
-  selectedUser = DUMMY_USERS[randomIndex]
+  //Aquí es donde hay que declarar el tipo de las variables de TypeScript.
+  //La exclamación dice que la variable no será nula.
+  @Input() avatar!: string;
+  @Input() name!: string;
 
   get imagePath() {
-    return `assets/users/${this.selectedUser.avatar}`;
-  }*/
-
-  //MÉTODO CON SIGNALS (favorece reacciones en cadena):
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  //computed asigna valores derivados de Signals, en este caso, la señal selectedUser.
-  imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);
-
+    return `assets/users/${this.avatar}`;
+  }
+ 
   onSelectUser(){
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+   
   }
 }
