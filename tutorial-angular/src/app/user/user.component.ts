173f6ce4907d0d4558c3ea5a3a45a5f2b490ebc9
javier <148455736+fjzamora93@users.noreply.github.com>
2024-06-01 20:38:04 +0200

    update

--- tutorial-angular/src/app/user/user.component.ts
+++ tutorial-angular/src/app/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+
+//Signals es una forma evolucionada de detectar eventos, frente a la clásica forma de hacerlo con métodos.
+import { Component, signal, computed } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -12,5 +14,22 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  
+  /*MÉTODO CLÁSICO (hace verificaciones manuales):
+   
   selectedUser = DUMMY_USERS[randomIndex]
+
+  get imagePath() {
+    return `assets/users/${this.selectedUser.avatar}`;
+  }*/
+
+  //MÉTODO CON SIGNALS (favorece reacciones en cadena):
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  //computed asigna valores derivados de Signals, en este caso, la señal selectedUser.
+  imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);
+
+  onSelectUser(){
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
 }
