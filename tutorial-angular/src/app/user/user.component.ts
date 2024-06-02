bcd7ba675b58f2d895fa8c53ed6aed7c7948206a
javier <148455736+fjzamora93@users.noreply.github.com>
2024-06-02 13:26:12 +0200

    Signals

--- tutorial-angular/src/app/user/user.component.ts
+++ tutorial-angular/src/app/user/user.component.ts
@@ -1,6 +1,7 @@
 
-import { Component, Input } from '@angular/core';
+import { Component, Input, input, computed } from '@angular/core';
 
+//Input es el decorator, input es una función.
 
 
 
@@ -15,13 +16,25 @@ export class UserComponent {
   //Aquí es donde hay que declarar el tipo de las variables de TypeScript.
   //La exclamación dice que la variable no será nula.
   @Input() avatar!: string;
-  @Input() name!: string;
 
+  //Sin el required, estamos diciendo una mentira. Ya que le decimos que el valor no será nulo, pero no es obligatorio.
+  @Input({required: true}) name!: string;
+
+  //Al igual que en Python, uno puede acceder a los getters sin los paréntesis al llamar a la función.
   get imagePath() {
     return `assets/users/${this.avatar}`;
   }
- 
+
+
+/*ENFOQUE CON SIGNALS
+
+  avatarSignals = input<String>('valor por defecto'); //Con valor por defecto porque no es obligatorio
+  nameSignals = input.required<String>(); //Sin valor por defecto porque es obligatorio
+  imagePathSignals = computed(() => {
+    return `assets/users/${this.avatarSignals()}`;
+    });
+*/
   onSelectUser(){
-   
+    console.log(`User ${this.name} selected`);
   }
 }
