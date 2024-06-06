261e26f7dbcf3d2d66bc32398b77b0140415ec7a
fjzamora93 <fjzamora93@gmail.com>
2024-06-06 13:53:33 +0200

    add tasks

--- tutorial-angular/src/app/tasks/new-task/new-task.component.ts
+++ tutorial-angular/src/app/tasks/new-task/new-task.component.ts
@@ -1,20 +1,29 @@
 
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { type Task } from '../task/task.model';
+import { Component, Input, Output, EventEmitter, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 
 export class NewTaskComponent {
-  @Output() select = new EventEmitter<void>();
+  @Output() cancel = new EventEmitter<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+
+  //Enfoque con signal: solo hay que meter la función, no cambia nada en el HTML
+  enteredDueDate = signal('');
 
   onCancel(){
     console.log('Emitimos un evento, pero no hacemos nada más. Es al recibir el evento cuando se activa la función que cerrará');
-    this.select.emit();
+    this.cancel.emit();
+  }
+
+  onSubmit(){
+    console.log('Se ha enviado el formulario al cliente: ', this.enteredTitle, this.enteredSummary, this.enteredDueDate);
   }
 }
