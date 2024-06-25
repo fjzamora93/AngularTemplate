270b48a939822260dbc824998c6e9d178109cad7
javier <148455736+fjzamora93@users.noreply.github.com>
2024-06-25 12:07:14 +0200

    update

--- tutorial-angular/src/app/app.component.ts
+++ tutorial-angular/src/app/app.component.ts
@@ -1,30 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
-import { TasksComponent } from './tasks/tasks.component';  
-import { DUMMY_USERS } from './dummy-users';
+
+import { AuthComponent } from './auth/auth.component';
+import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [AuthComponent, LearningResourcesComponent],
 })
-export class AppComponent {
-  //Podemos exportar desde Components al resto de componentes desde aquí
-  users = DUMMY_USERS;
-
-  selectedUserId?: string;
-
-  get selectedUser() {
-    return this.users.find((user) => user.id === this.selectedUserId); //Si pusiéramos una ! al final sería que va a encontrar algún resultado sí o sí.
-  }
-
-  //Si te fijas, en user.component tienes un método que se llama onSelectUser, que emite un evento. Esto es lo que asignará el valor de selectedUserId.
-  onSelectUser(id: string) {
-    console.log('User selected AHORA', id)
-    this.selectedUserId = id;
-  }
-}
+export class AppComponent {}
--- tutorial-angular/src/main.ts
+++ tutorial-angular/src/main.ts
@@ -1,26 +1,5 @@
-
-
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+
 import { AppComponent } from './app/app.component';
 
-
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-
-
-
-
-//!CÓMO QUEDARÍA EL CÓDIGO EN CASO DE USAR MÓDULOS:
-
-/**
- * import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';  
- * import { AppModule } from './app/app.module';
- * 
- * platformBrowserDynamic().bootstrapModule(AppModule);
- * 
- */
-
-
-//!Y hasta aquí el documento main.ts
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
